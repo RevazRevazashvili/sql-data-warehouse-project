4b9c11034248fa17d6d860d382a0de24bf520613
RevazRevazashvili <revazi.revazashvili380@ens.tsu.edu.ge>
2025-07-04 16:57:07 +0400

    added stored procedure to proc_load_bronze.sql file and added data flow diagram

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,53 +1,127 @@
-TRUNCATE TABLE bronze.crm_cust_info;
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @start DATETIME, @end DATETIME;
+	SET @start = GETDATE()
+	BEGIN TRY
+		PRINT '============================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '============================================';
 
-TRUNCATE TABLE bronze.crm_prd_info;
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+		PRINT '--------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '--------------------------------------------';
 
-TRUNCATE TABLE bronze.crm_sales_details;
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+		SET @start_time = GETDATE()
+		PRINT '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
 
-TRUNCATE TABLE bronze.erp_cust_az12;
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-WITH(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+		PRINT '>> Inserting Dara Inot: bronze.crm_cust_info';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE()
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds'
 
-TRUNCATE TABLE bronze.erp_loc_a101;
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-WITH(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+		SET @start_time = GETDATE()
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-WITH(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+		PRINT '>> Inserting Dara Inot: bronze.crm_prd_info';
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE()
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+
+		SET @start_time = GETDATE()
+		PRINT '>> Truncating Table: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
+
+		PRINT '>> Inserting Dara Inot: bronze.crm_sales_details';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE()
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+
+
+		PRINT '--------------------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '--------------------------------------------';
+
+		SET @start_time = GETDATE()
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+
+		PRINT '>> Inserting Dara Inot: bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE()
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+
+		SET @start_time = GETDATE()
+		PRINT '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+
+		PRINT '>> Inserting Dara Inot: bronze.erp_loc_a101';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE()
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+
+
+		SET @start_time = GETDATE()
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+		PRINT '>> Inserting Dara Inot: bronze.erp_px_cat_g1v2';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\revaz\Desktop\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE()
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds'
+
+	END TRY
+
+	BEGIN CATCH
+		PRINT '============================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'Error Message ' + ERROR_MESSAGE();
+		PRINT 'Error Message ' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message ' + CAST(ERROR_STATE() AS NVARCHAR);
+		PRINT '============================================';
+	END CATCH
+	SET @end = GETDATE()
+
+	PRINT '============================================';
+	PRINT 'Bronze Layer Full Load Time: '+ CAST(DATEDIFF(SECOND, @start, @end) AS NVARCHAR) + ' seconds';
+	PRINT '============================================';
+END
+
+EXEC bronze.load_bronze;
